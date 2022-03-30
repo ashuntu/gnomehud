539453f6b3e0109142f99f05c18047167bdab78c
ashtoncano <101582426+ashtoncano@users.noreply.github.com>
2022-03-30 15:29:39 -0500

    Path updates

---
+++ prefs.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const { Adw, Gio, Gtk } = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+function init()
+{
+
+}
+
+function fillPreferencesWindow(window)
+{
+    const settings = ExtensionUtils.getSettings("org.gnome.shell.extensions.gnomehud");
+
+    const page = new Adw.PreferencesPage();
+    const group = new Adw.PreferencesGroup();
+    page.add(group);
+
+    const row = new Adw.ActionRow({ title: "Show Extension Indicator" });
+    group.add(row);
+
+    const toggle = new Gtk.Switch({
+        active: settings.get_boolean("show-indicator"),
+        valign: Gtk.Align.CENTER,
+    });
+
+    settings.bind(
+        "show-indicator",
+        toggle,
+        "active",
+        Gio.SettingsBindFlags.DEFAULT,
+    );
+
+    row.add_suffix(toggle);
+    row.activatable_widget = toggle;
+
+    window.add(page);
+}
